cac7767d56c18376a34487274f41c85071d17fc4
Orphieus <yatharthnehradpspune@gmail.com>
2026-03-28 12:25:03 +0530

    Code updates to show experiments

--- proj1_todo.py
+++ proj1_todo.py
@@ -1,7 +1,7 @@
 x1 = input("Enter todo number 1:\n")
 x2 = input("Enter todo number 2:\n")
 x3 = input("Enter todo number 3:\n")
-todo_list = [x1, x2, x3]
+todo_list = [x1, x2, x3, "Hello"]
 print(todo_list)
 
 print(type(todo_list))
