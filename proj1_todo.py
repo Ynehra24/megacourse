784ad38f45181a05bbbb0c8c0cb80222f4caa472
Orphieus <yatharthnehradpspune@gmail.com>
2026-06-06 23:51:30 +0530

    current code

--- proj1_todo.py
+++ proj1_todo.py
@@ -1,13 +1,8 @@
-x1 = input("Enter todo number 1:\n")
-x2 = input("Enter todo number 2:\n")
-x3 = input("Enter todo number 3:\n")
-todo_list = [x1, x2, x3, "Hello"]
-print(todo_list)
+todo = []
+while True:
+    todo_element = input("Enter a task or done to finish: ")
+    if(todo_element == "exit"):
+        break
+    todo.append(todo_element)
 
-print(type(todo_list))
-
-def counting_chars():
-    userip = input("Enter title:\n")
-    return len(userip)
-
-print(counting_chars())
+print("Today's list ", todo)
