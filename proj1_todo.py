d6bf0144a4644241a7ac67744bfde9d803ae7199
Orphieus <yatharthnehradpspune@gmail.com>
2026-06-08 23:13:44 +0530

    Added enumeration and also the task completed feature

--- proj1_todo.py
+++ proj1_todo.py
@@ -1,14 +1,14 @@
 todo = []
 while True:
-    todo_element = input("Enter an action(add/show/completed/exit): ")
+    todo_element = input("Enter an action(add/show/edit/completed/exit): ")
     match todo_element:
         case "add":
             elem = input("Enter the item: ")
             elem.strip()
             todo.append(elem)
         case "show":
-            for elem in todo:
-                print(elem)
+            for ind, elem in enumerate(todo):
+                print(ind + 1, '.', elem)
         case "completed":
             inp = input("Which task did you finish? ")
             if inp in todo:
@@ -16,8 +16,15 @@ while True:
                 print("You have completed task ", inp)
             else:
                 print("Error: Task not found")
-
+        case "edit":
+            num = int(input("Which To-do iteam would you like to edit? "))
+            if(num < len(todo)):
+                new_item = input("Enter the new To-do item: ")
+                print("Changed the task ", todo[num], "to", new_item)
+                todo[num] = new_item
         case "exit":
             break
+        case _:
+            print("Error: choose an actual choice from the list please")
 
 print("Remaining list ", todo)
